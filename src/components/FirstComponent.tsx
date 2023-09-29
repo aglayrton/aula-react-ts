69661b252253d21d7b2cb15d8159cf3b6b29241c
aglayrton <aglayrtonjuliao@gmail.com>
2023-09-28 22:16:50 -0300

    Componentes

---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+function FirstComponent() {
+  return (
+    <div>
+      <h1>Meu primeiro componente</h1>
+    </div>
+  );
+}
+
+export default FirstComponent;
+
+/** 
+ * export const FirstComponent : React.FC = () =>{
+    return <div>FirstComponent</div>
+}
+
+ * export function FirstComponent2(){
+    return <div>FirstComponent</div>
+}*/
