538d702227535d9a34dd026ab34e06e68d3346f3
aglayrton <aglayrtonjuliao@gmail.com>
2023-09-28 22:07:02 -0300

    Função

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ function App() {
   //1 - variáveis
   const name: string = "Julião";
   const isWorking: boolean = true;
+  //2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}`;
+  };
   return (
     <div className='App'>
       <h1>{name}</h1>
       {isWorking && <p>Está trabalhando</p>}
+      <h3>{userGreeting("Jotinha")}</h3>
     </div>
   );
 }
